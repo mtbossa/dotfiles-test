8008f67d00d87aeb83f56e1d5f2ed08db647f6e0
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2023-04-03 21:13:33 -0300

    Add alises

---
+++ oh-my-zsh-customization/aliases.zsh
@@ -0,0 +1,7 @@
+alias cl="clear"
+alias lzg="lazygit"
+alias sud="sail up -d"
+alias sail="[ -f sail ] && bash sail || bash vendor/bin/sail"
+alias sailrestart="sail down && sud"
+alias pf="clear && sail test --filter"
+alias sailrestart="sail down && sud"
