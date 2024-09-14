ffea3fd6aae746ae66e2e556e4a0b007a95538c4
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2024-09-14 17:40:03 -0300

    Small changes

--- oh-my-zsh-customization/composer_install_docker.zsh
+++ oh-my-zsh-customization/composer_install_docker.zsh
@@ -3,6 +3,6 @@ function composer_install_docker() {
     -u "$(id -u):$(id -g)" \
     -v "$(pwd):/var/www/html" \
     -w /var/www/html \
-    laravelsail/php${1:-82}-composer:latest \
+    laravelsail/php${1:-83}-composer:latest \
     composer install --ignore-platform-reqs
 }
